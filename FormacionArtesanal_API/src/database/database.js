6932b08c041288f2f2b8eda113a111cd23d40d0e
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2023-07-18 17:07:03 -0500

    Configuracion de footer

--- FormacionArtesanal_API/src/database/database.js
+++ FormacionArtesanal_API/src/database/database.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import { createConnection } from "mysql2/promise";
 dotenv.config();
 const connection = await createConnection({
-	host: 'localhost',
+	host: '192.60.1.216',
 	user: 'root',
 	password: '12345',
 	database: 'ug_artesanal',
